9cfa244d6e0ee185d3c6f3d73d56a501c8d6121b
vk92kokil <vk92kokil@gmail.com>
2020-04-03 10:19:58 +0530

    add socket library

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import App from 'next/app'
-import { appWithTranslation } from '../i18n'
+import App from 'next/app';
+import io from 'socket.io-client';
+import { appWithTranslation } from '../i18n';
+
 
 class MyApp extends App {
+  componentDidMount() {
+    const socket = io();
+    socket.on('connection', (socket) => {
+      console.log("Connected with server");
+    });
+    socket.on('update', () => {
+        
+    });
+  }
   render() {
     const { Component, pageProps } = this.props
     return (
