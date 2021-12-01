d32fe96758108974ad61d266c3da0904e9f28a99
Amrendra Nath <amrendranath@Amrendras-MacBook-Air.local>
2021-12-01 22:01:13 +0530

    fixissu

--- i18n.js
+++ i18n.js
@@ -2,7 +2,7 @@ const NextI18Next = require("next-i18next").default;
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 
 const path = require('path');
-path.resolve('./public/static/locales/');
+path.resolve('/public/static/locales/');
 
 module.exports = new NextI18Next({
   defaultLanguage: "en",
