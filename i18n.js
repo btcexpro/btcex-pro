f18bad9253857238b235aefe171edb5638fef656
Amrendra Nath <amrendranath@Amrendras-MacBook-Air.local>
2021-12-01 22:06:01 +0530

    added path

--- i18n.js
+++ i18n.js
@@ -2,13 +2,10 @@ const NextI18Next = require("next-i18next").default;
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 
 const path = require('path');
-path.resolve('/public/static/locales/');
 
 module.exports = new NextI18Next({
   defaultLanguage: "en",
   otherLanguages: ["zh"],
   strictMode: false,
-  // workaround until next-i18next support public path
-  // https://github.com/isaachinman/next-i18next/issues/523
-  // localePath: typeof window === "undefined" ? "/public/static/locales" : "/public/static/locales"
+  localePath: path.resolve('/public/static/locales')
 });
